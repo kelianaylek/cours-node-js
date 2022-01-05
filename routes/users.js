71e704d2a58040f2eaf770f4c783877738de980c
Kélian <kelian.raoult_severac@edu.devinci.fr>
2022-01-05 14:56:16 +0100

    Add prisma config and user entity

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+const { PrismaClient } = require('@prisma/client')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const prisma = new PrismaClient()
+
+const app = express();
+
+/* GET users. */
+app.get('/', async function(req, res, next) {
+  const users = await prisma.users.findMany()
+  res.status(200).json(users);
 });
 
-module.exports = router;
+/* POST users. */
+app.post('/create', async (req, res) => {
+  const { username, password, email } = req.body
+  const user = await prisma.users.create({
+    data: {
+      username,
+      password,
+      email,
+      created_on : new Date(),
+      last_login : new Date()
+    },
+  })
+  res.json(user)
+})
+
+module.exports = app;
