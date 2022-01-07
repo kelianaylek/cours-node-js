f1039a895d5f040a6a1f732bd3358e7706246c0a
Kélian <kelian.raoult_severac@edu.devinci.fr>
2022-01-07 14:15:36 +0100

    Try to add socket io in production

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
 io.on('connection', (socket) => {
   socket.on('chat message', (msg) => {
     console.log('message: ' + msg);
